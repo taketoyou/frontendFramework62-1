2ad45c743dcd2c2011019811dcdd21864cf9e01b
taketoyou <www.taketoyou.com@gmail.com>
2019-05-02 09:54:34 +0700

    Test deploy to Heroku.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// Serve only the static files form the dist directory app.use(express.static('./dist/frontendFramework62'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/frontendFramework62'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/frontendFramework62/index.html'));
 });
